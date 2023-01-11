6d7d39421c9451cab288687414d361b8dece4ae4
Elena <elmikhailova86@gmail.com>
2023-01-11 14:23:37 +0300

    some changes

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,43 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    @State var title: String = "NEXT"
     var body: some View {
+        
         ZStack{
-            Color.white
+            Color.black
                 .edgesIgnoringSafeArea(.all)
             VStack{
                 VStack {
                     Circle()
                         .foregroundColor(.red)
+                        .opacity(lightIsOff)
                         .frame(width: 150, height: 150)
                         .overlay(Circle().stroke(Color.white, lineWidth: 4))
                     
                     Circle()
                         .foregroundColor(.yellow)
+                        .opacity(lightIsOff)
                         .frame(width: 150, height: 150)
                         .overlay(Circle().stroke(Color.white, lineWidth: 4))
                         .padding(10)
                     Circle()
                         .foregroundColor(.green)
+                        .opacity(lightIsOff)
                         .frame(width: 150, height: 150)
                         .overlay(Circle().stroke(Color.white, lineWidth: 4))
                     
                     
                 }
-                .padding(0.0)
-    
+                .padding()
+                Spacer()
                 
-                Button("Text", action: {})
+                Button(action: {}) {
+                    Text("START".uppercased())
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding()
+                        .background(
+                            Color.blue
+                            .cornerRadius(15)
+                        )
+                    }
                 
-            }
-            
+                }
         }
-      
     }
+    //                func startButtonPressed() {
+    //                    if  == "START" {
+    //                        Text("NEXT")
+    //                    }
+    //            enum CurrentLight {
+    //                case red, yellow, green
+    //            }
+    //            switch currentLight {
+    //            case .red:
+    //                    .opacity(lightIsOff)
+    //                redLight.alpha = lightIsOn
+    //                currentLight = .yellow
+    //            case .yellow:
+    //                redLight.alpha = lightIsOff
+    //                yellowLight.alpha = lightIsOn
+    //                currentLight = .green
+    //            case .green:
+    //                greenLight.alpha = lightIsOn
+    //                yellowLight.alpha = lightIsOff
+    //                currentLight = .red
+    //            }
+    //        }
     
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
